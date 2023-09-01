4092f1beb94eaf8a65b35d40763807f9d188436c
guledabdilatif <guledabdilatif@gmail.com>
2023-09-01 00:33:25 -0700

    Updated portfolio component, web and design section

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./Portfolio.css";
-import { portfolioDetails } from "./portfolioDetails";
+import { WebsitesDetails, DesignsDetails } from "./portfolioDetails";
 
 const Portfolio = () => {
   const [toggle, setToggle] = useState(1);
@@ -19,29 +19,37 @@ const Portfolio = () => {
         <button onClick={() => updateToggle(2)}>Designs</button>
         <button onClick={() => updateToggle(3)}>Mobile Apps</button>
         <div className={toggle === 1 ? "show-content" : "content"}>
+          {/* web */}
           <div className="web-rows text-center">
-            {portfolioDetails.map((item) => {
+            {WebsitesDetails.map((item) => {
               return <div>
                 <div className="web-row-container">
                   <img src={item.webimage} alt="" />
-                  <div className="web-details">
+                  <div className="web-details ">
                     <p>{item.webname}</p>
-                    <a href={item.weblink}>{item.view}</a>
+                    <div style={{ display: 'flex', gap: 3, alignItems: 'center' }}>
+                      <a href={item.weblink} target="_blank" className="anchor">{item.view}</a>
+                      <a href={item.GithubLink} target="_blank" style={{ fontSize: 24, marginLeft: 10, marginTop: -10, textDecoration: 'none' }}>{item.GithubIcon}</a>
+                    </div>
                   </div>
                 </div>
               </div>
             })}
           </div>
         </div>
+        {/* designs */}
         <div className={toggle === 2 ? "show-content" : "content"}>
           <div className="web-rows">
-            {portfolioDetails.map((item) => {
+            {DesignsDetails.map((item) => {
               return <div>
                 <div>
                   <img src={item.desingimage} alt="" />
                   <div className="web-details">
                     <p>{item.desingname}</p>
-
+                    <div>
+                      <a href={item.downloaddesign} target="_blank" className="anchor" download>Download</a>
+                      <a href={item.figmafileLink} target="_blank" style={{ fontSize: 24, marginLeft: 10, marginTop: -10, textDecoration: 'none' }}>{item.figmaIcon}</a>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -49,6 +57,7 @@ const Portfolio = () => {
           </div>
 
         </div>
+        {/* mobile apps */}
         <div className={toggle === 3 ? "show-content" : "content"}>
           <h1>Mobile Apps</h1>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et facilis tenetur perferendis explicabo cupiditate saepe libero sapiente culpa, doloribus possimus? Ratione facilis placeat quam vitae tempora. Est hic omnis aliquid.F</p>
--- src/Components/Portfolio/portfolioDetails.js
+++ src/Components/Portfolio/portfolioDetails.js
@@ -3,34 +3,110 @@ import coffee from "../../Assets/portfolio1.png";
 import TourandTravelWebsite from "../../Assets/portfolio2.png";
 import FoodWebsite from "../../Assets/portfolio3.png";
 import FoodWebDesign from "../../Assets/portfolio4.png";
-import  WebsiteDesign from "../../Assets/portfolio5.png";
 import PCLoginDesign from "../../Assets/portfolio6.png";
+import mobileLoginUI from "../../Assets/mobile-login-ui.png";
+import SignUP from "../../Assets/Sign-Up.png";
+import Checkout from "../../Assets/Checkout.png";
+import eBike from "../../Assets/e-Bike-Web-UI.png";
+import { AiFillGithub } from 'react-icons/ai'
+import { FiFigma } from 'react-icons/fi'
 
+// Web Links
 var coffeeLink = "https://caffeine-site.netlify.app";
+var coffeeGithubLink = "https://github.com/guledabdilatif/Coffee-Website";
 var TourandTravelWebsiteLink = "https://travel-travel-site.netlify.app";
+var TourandTravelGithubLink = "https://github.com/guledabdilatif/tour-and-travel-website";
 var FoodWebsiteLink = "https://refreshment.netlify.app";
+var FoodWebsiteGithubLink = "https://github.com/guledabdilatif/Food-website";
 
-export const portfolioDetails= [
+//Web file Link
+// var YouTubeLink = "https://github.com/guledabdilatif/Food-website";
+// var facebookLink = "https://github.com/guledabdilatif/Food-website";
+// var WeatherApp = "https://github.com/guledabdilatif/Food-website";
+// var YouTubeGithubLink = "https://github.com/guledabdilatif/You-Tube-Clone";
+// var WeatherAppGithubLink = "https://github.com/guledabdilatif/weather-app";
+// var facebookGithubLink = "https://github.com/guledabdilatif/facebook-clone";
+
+// Design file Link
+var FoodDesignFigmaFile = "https://www.figma.com/file/svNxChr2lqChjwjDTGcjSO/Food-web-App-Design?type=design&node-id=0-1&mode=design&t=INYtwN4CHhFtWKpu-0"
+var LoginDesignPCfigmaLink = "https://www.figma.com/file/IeTsYdYhKgcUw9uUKX6lvt/Login-iMac-1?type=design&node-id=6-15&mode=design&t=8mUEk4zNLbtnTVrI-0"
+var eBikeFigmaFile = "https://www.figma.com/file/DDQA1aMQkjzOKiQ1fTLjzE/e-Bike-landing-page-UI?type=design&node-id=2-2&mode=design&t=TULHXHCvPrwdi4L7-0"
+var checkOutFigmaFile = "https://www.figma.com/file/YQvbjSExU7ttRRWMwPuCY5/Checkout?type=design&node-id=1-2&mode=design&t=Zf9fAn8BH9sMhE6G-0"
+var SignUPFigmaFile = "https://www.figma.com/file/pp31dOwNYijZxQbsln8Aca/Sign-Up?type=design&node-id=1-2&mode=design&t=JBagjJarFrl4cruI-0"
+var LoginMobileFigmaFile = "https://www.figma.com/file/fUJ65ylKSkAjPggsf8D5Qc/login-mobile-page?type=design&node-id=1-4&mode=design&t=svJwE7D677Wxe0cY-0"
+
+
+export const DesignsDetails = [
+    {
+        figmaIcon: <FiFigma />,
+        figmafileLink: FoodDesignFigmaFile,
+        desingname: "Food Website Design",
+        downloaddesign: FoodWebDesign,
+        desingimage: FoodWebDesign,
+    },
+    {
+        figmaIcon: <FiFigma />,
+        figmafileLink: eBikeFigmaFile,
+        desingname: "e-Bike Landing Page",
+        downloaddesign: eBike,
+        desingimage: eBike,
+    },
+    {
+        figmaIcon: <FiFigma />,
+        figmafileLink: LoginDesignPCfigmaLink,
+        downloaddesign: FoodWebDesign,
+        desingname: " PC Login Design",
+        desingimage: PCLoginDesign,
+    },
+    {
+        figmaIcon: <FiFigma />,
+        figmafileLink: checkOutFigmaFile,
+        downloaddesign: Checkout,
+        desingname: " Checkout",
+        desingimage: Checkout,
+    },
+    {
+        figmaIcon: <FiFigma />,
+        figmafileLink: SignUPFigmaFile,
+        downloaddesign: SignUP,
+        desingname: " Sign-Up",
+        desingimage: SignUP,
+    },
+    {
+        figmaIcon: <FiFigma />,
+        figmafileLink: LoginMobileFigmaFile,
+        downloaddesign: mobileLoginUI,
+        desingname: " Mobile Login UiI",
+        desingimage: mobileLoginUI,
+    },
+]
+export const WebsitesDetails = [
     {
         webname: "Coffee",
         webimage: coffee,
         weblink: coffeeLink,
-        view:"view",
-        desingname: "Food Website Design",
-        desingimage: FoodWebDesign,
+        view: "Demo",
+        GithubIcon: <AiFillGithub />,
+        GithubLink: coffeeGithubLink
     },
     {
         webname: "Tour and Travel Website",
         webimage: TourandTravelWebsite,
         weblink: TourandTravelWebsiteLink,
-        view:"view",
-        desingname: " PC Login Design",
-        desingimage:  PCLoginDesign,
+        view: "Demo",
+        GithubIcon: <AiFillGithub />,
+        GithubLink: TourandTravelGithubLink,
     },
     {
         webname: "Food Website",
         webimage: FoodWebsite,
         weblink: FoodWebsiteLink,
-        view:"view"
-    }
+        view: "Demo",
+        GithubIcon: <AiFillGithub />,
+        GithubLink: FoodWebsiteGithubLink,
+    },
+]
+export const portfolioDetails = [
+
+
 ]
